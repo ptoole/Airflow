e334d034d4e7a482f953fa48a8e70e31ec3e547b
Patrick Toole <ptoole@users.noreply.github.com>
2018-08-21 19:54:48 -0400

    Initial implementation.

---
+++ Example.py
@@ -0,0 +1,75 @@
+{
+  "paragraphs": [
+    {
+      "text": "\nprint(\"This line will be printed.\")",
+      "user": "ptoole@qubole.com",
+      "dateUpdated": "Aug 21, 2018 11:49:59 PM",
+      "config": {
+        "colWidth": 12.0,
+        "graph": {
+          "mode": "table",
+          "height": 300.0,
+          "optionOpen": false,
+          "keys": [],
+          "values": [],
+          "groups": [],
+          "scatter": {}
+        },
+        "enabled": true,
+        "editorMode": "ace/mode/scala"
+      },
+      "settings": {
+        "params": {},
+        "forms": {}
+      },
+      "paragraphProgress": {
+        "jobs": [],
+        "numCompletedTasks": 0,
+        "numTasks": 0,
+        "truncated": false
+      },
+      "version": "v0",
+      "jobName": "paragraph_1534894073199_-1706752487",
+      "id": "20180821-232753_1144993508_q_PWB91R1ET31534893758",
+      "result": {
+        "code": "SUCCESS",
+        "type": "TEXT",
+        "msg": "This line will be printed."
+      },
+      "dateCreated": "Aug 21, 2018 11:27:53 PM",
+      "dateSubmitted": "Aug 21, 2018 11:49:59 PM",
+      "dateStarted": "Aug 21, 2018 11:50:01 PM",
+      "dateFinished": "Aug 21, 2018 11:50:51 PM",
+      "status": "FINISHED",
+      "progressUpdateIntervalMs": 1000
+    },
+    {
+      "text": "",
+      "config": {},
+      "settings": {
+        "params": {},
+        "forms": {}
+      },
+      "version": "v0",
+      "jobName": "paragraph_1534895399224_-1314404964",
+      "id": "20180821-234959_1380342676_q_PWB91R1ET31534893758",
+      "dateCreated": "Aug 21, 2018 11:49:59 PM",
+      "status": "READY",
+      "progressUpdateIntervalMs": 1000
+    }
+  ],
+  "name": "Example.py",
+  "id": "PWB91R1ET31534893758",
+  "angularObjects": {
+    "2D6S4V6R633551517332493711:shared_process": [],
+    "2D4NVQY3Z33551517332493696:shared_process": [],
+    "2D5MTWVG233551517332493673:shared_process": [],
+    "2D6UDAR6A33551517332493703:shared_process": []
+  },
+  "config": {
+    "isDashboard": false,
+    "defaultLang": ""
+  },
+  "info": {},
+  "source": "FCN"
+}
